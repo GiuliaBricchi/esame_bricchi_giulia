83726baaab7d95bcd1c6a797f6977caf7fe2fe60
Giulia <bricchigiulia6@gmail.com>
2024-03-05 07:30:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+//loader
+var loader = document.getElementById("preloader");
+
+
+setTimeout(function() {
+  loader.style.display = "none";
+}, 2000);
+
+//menu
+const menu = document.querySelector('#menu')
+const toggle = document.querySelector('#toggle')
+
+const toggleMenu = () => {
+  toggle.classList.toggle('is-active')
+  menu.classList.toggle('active')
+}
+
+document.addEventListener('DOMContentLoaded', () =>{
+  toggle.addEventListener('click', toggleMenu)
+})
+
+
+
+//read more
+$(document).ready(function() {
+  $("#read-more-btn").click(function() {
+    $("#hidden-text").toggleClass("d-none");
+    $(this).text(function(i, text){
+      return text === "Read More" ? "Read Less" : "Read More";
+    });
+  });
+});
+
+
+//scrolltrigger
+const anim = () => {
+  const vwidth = window.innerWidth; 
+  const message1 = document.getElementById("message1");
+
+  const tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: "#headline",
+      pin: true, 
+      start: "top 100px", 
+      scrub: true,
+      markers: false,
+      onEnter: () => { 
+        console.log("enter");
+      },
+      onLeave: () => { 
+        console.log("leave");
+      }
+    }
+  });
+
+  tl2.from(message1, 1, { x: -vwidth });
+
+  message1.addEventListener("click", () => {
+    gsap.set(message1, { x: 100 });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", anim);
